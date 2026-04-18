9a008da3a1a65272ad28f92aa818521658aec403
Krzyś <krzysrog@tlen.pl>
2026-04-18 23:01:08 +0200

    wspolne

---
+++ shared/map256.asm
@@ -0,0 +1,24 @@
+        icl "ANTIC"
+        icl "DOS"
+        icl "GTIA"
+        icl "KEYS"
+        icl "OS"
+        icl "PIA"
+        icl "POKEY"
+
+
+        osgraph = $ef9c ;<A
+        osplot = $f1d8  ;<atachr
+        osprint = $f1a4 ;
+        osdrawto = $f9c2
+        locate = $f18f  ;>A
+
+        convrt = $f5ac
+        ;adress = $64
+        ;atachr = $2fb
+        bell = $f556
+        keyclk = $f983
+        ;kgetch = $f302
+        egetch = $f24a
+        kgetch = $f302
+    
